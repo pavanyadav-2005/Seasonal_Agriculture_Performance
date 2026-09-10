be7af4516d98862eb117535a08df54b262b8c124
pavanyadav-2005 <247r5a0513@cmrtc.ac.in>
2026-09-10 12:33:04 +0530

    Add files via upload

---
+++ pandas_test.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+import pandas as pd 
+data={
+    "states":["AP","Gujarat","Karnataka","Madhya Pradesh","Maharastra","punjab","Tamilnadu","Telangana"],
+    "values":[19527.06,23586.01,23341.3,18667.08,22519.83,25280.82,19807.58,20255.16],
+}
+df=pd.DataFrame(data)
+print(df)
